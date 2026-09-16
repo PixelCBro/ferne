7f39dc3e3a542b61a0c5d36904dbfaab6bcf77e2
CBro <zf48p7zsrn@privaterelay.appleid.com>
2026-09-16 13:24:33 +0200

    Add Android app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "de.chris.samsungremote"
+    compileSdk = 37
+
+    defaultConfig {
+        applicationId = "de.chris.samsungremote"
+        minSdk = 26
+        targetSdk = 37
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:5.2.1"))
+    implementation("com.squareup.okhttp3:okhttp")
+}
