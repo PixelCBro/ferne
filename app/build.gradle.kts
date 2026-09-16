8c1c689f5a815c316c794acbcf926ab3e8ef7c82
CBro <zf48p7zsrn@privaterelay.appleid.com>
2026-09-16 13:30:53 +0200

    Target Android 16 for stable CI build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "de.chris.samsungremote"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "de.chris.samsungremote"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
     }
